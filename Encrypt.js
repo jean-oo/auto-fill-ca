1a7eaf6996ae920bfa2862dc0a79c5f1f1bb0e2e
jean-oo <76278882+jean-oo@users.noreply.github.com>
2025-04-04 16:08:45 -0700

    Update Encrypt.js

--- Encrypt.js
+++ Encrypt.js
@@ -2,8 +2,9 @@ const crypto = require('crypto');
 const path = require('path');
 const fs = require('fs');
 
-function encrypt(text, key) {
-    const cipher = crypto.createCipher('aes-256-cbc', key); //create an encryption object
+function encrypt(text, key，iv) {
+    const cipher = crypto.createCipher('aes-256-cbc', key,iv); //create an encryption object
+    cipher.setAutoPadding(true); 
     let encrypted = cipher.update(text, 'utf8', 'hex'); // encrypt the text, in hex format
     encrypted += cipher.final('hex');
     return encrypted;
@@ -14,8 +15,8 @@ const code = fs.readFileSync(
         { encoding: 'utf8' }
     );
 const encryptionKey = crypto.randomBytes(32);  
-const encryptedCode = encrypt(code, encryptionKey);
 const iv = crypto.randomBytes(16); // apply to CBC mode, when the input is the same, will generate different encryption results to enhance security 
+const encryptedCode = encrypt(code, encryptionKey,iv);
 
 fs.writeFileSync(
         path.join(__dirname, 'encrypted.js'),
