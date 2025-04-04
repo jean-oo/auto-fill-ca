16268e8b819c3dc280207b08441a5a4fd7ac902f
jean-oo <76278882+jean-oo@users.noreply.github.com>
2025-04-04 16:04:13 -0700

    Update Encrypt.js

--- Encrypt.js
+++ Encrypt.js
@@ -9,32 +9,21 @@ function encrypt(text, key) {
     return encrypted;
 }
 
-const publicDir = path.join(__dirname, 'public');
-if (!fs.existsSync(publicDir)) {
-    fs.mkdirSync(publicDir);
-}
-
-try {
-    const code = fs.readFileSync(
-        path.join(__dirname, 'src/core-script.js'),
+const code = fs.readFileSync(
+        path.join(__dirname, 'core-script.js'),
         { encoding: 'utf8' }
     );
-    
-const coreScript = fs.readFileSync(
-    path.join(__dirname, 'core-script.js'),
-    { encoding: 'utf8' }
-);
-const encryptionKey = crypto.randomBytes(32);  // Change this key
-const encryptedCode = encrypt(cireScript, encryptionKey);
+const encryptionKey = crypto.randomBytes(32);  
+const encryptedCode = encrypt(code, encryptionKey);
 const iv = crypto.randomBytes(16); // apply to CBC mode, when the input is the same, will generate different encryption results to enhance security 
 
 fs.writeFileSync(
-        path.join(publicDir, 'encrypted.js'),
+        path.join(__dirname, 'encrypted.js'),
         `${iv.toString('hex')}:${encrypted}`
     );
     
     fs.writeFileSync(
-        path.join(publicDir, 'key.txt'),
+        path.join(__dirname, 'key.txt'),
         key.toString('hex')
     );
 console.log("Encrypted Code:", encryptedCode);
